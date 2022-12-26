3f7f135c7cfe7b839d035ffd6b1509d46c351fb7
cassio645 <cantonio645@gmail.com>
2022-12-26 10:09:21 -0300

    primeiro projeto java

---
+++ primeiro-projeto/src/App.java
@@ -0,0 +1,8 @@
+import java.net.SocketPermission;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Bem-vindos turma DIO 2022");
+    }
+}
